9ed1f224cdda999b8a9be5849a798fa8ccd1c092
Yu Myungjae <u4bi@letsee.io>
2017-04-06 17:45:49 +0900

    코어 아키텍쳐 구성
    - src/core/App.js

---
+++ src/core/App.js
@@ -0,0 +1,16 @@
+/**
+ * @u4bi
+ * @Core App
+ */
+
+require('../sass/Basic.scss');
+
+var 
+    Basic;
+
+Basic = require('../modules/Basic');
+
+var U4bi = {};
+U4bi.Basic = Basic;
+
+module.exports = U4bi;
